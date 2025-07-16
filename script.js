55de270d945e25b571fa817ef191a0c4308e3fbf
Fatima <fatima.haider@hellopaisa.co.uk>
2025-07-16 13:50:04 +0400

    new updates

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = `I love bringing ideas to life through
+ code — solving tough problems, designing smart systems, and
+ creating tech that actually matters!`;
+
+const typedText = document.getElementById("typed-text");
+
+let index = 0;
+
+function typeChar() {
+  if (index < text.length) {
+    typedText.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeChar, 30); // Typing speed (ms per char)
+  }
+}
+
+window.onload = typeChar;
