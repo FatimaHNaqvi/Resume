e57fc5686ac5671f317a2d7e30049bea267766e2
Fatima <fatima.haider@hellopaisa.co.uk>
2025-07-16 17:18:43 +0400

    responsivesness

--- script.js
+++ script.js
@@ -15,3 +15,6 @@ function typeChar() {
 }
 
 window.onload = typeChar;
+  document.getElementById('menu-toggle').addEventListener('click', () => {
+    document.getElementById('nav-links').classList.toggle('show');
+  });
